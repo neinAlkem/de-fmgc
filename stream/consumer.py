0f1165856e19f4a9f3373672a45c212cbc76a6c8
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2025-11-16 18:53:36 +0700

    feat(stream): add kafka consumer script

---
+++ stream/consumer.py
@@ -0,0 +1,22 @@
+from confluent_kafka import Consumer
+import threading
+
+consumerConfig = {
+    'bootstrap.servers' : 'localhost:9092',
+    'group.id' : 'transaction',
+    'auto.offset.reset' : 'earliest'
+}
+
+consumer = Consumer(consumerConfig)
+consumer.subscribe(['posTransaction'])
+
+while True:
+    msg = consumer.poll(1.0)
+    if msg is None:
+         continue
+    elif msg.error():
+        print(f"Error: {msg.error()}")
+        continue
+    else:
+         print(f"Reciveing Message: {msg.value().decode('utf-8')}")
+
