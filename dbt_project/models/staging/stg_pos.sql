12f57f1947cb3f9d823a95a413687d85f5b33e1b
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2025-11-21 10:46:47 +0700

    feat(dbt): create stg_pos.sql

---
+++ dbt_project/models/staging/stg_pos.sql
@@ -0,0 +1,18 @@
+WITH stg_pos AS (
+    SELECT * FROM {{ source('raw', 'raw_pos') }}
+), rename AS( 
+    SELECT
+        CAST(transaction_id AS VARCHAR) AS transaction_id,
+        CAST(date_purchased AS DATE) AS date_purchases,
+        CAST(area_code AS CHAR(3)) AS area_code,
+        CAST(store_id AS CHAR(8)) AS store_id,
+        CAST(product_id AS INT) AS product_id, 
+        CAST(quantity as INT) as quantity,
+        CAST(unit_price AS FLOAT) AS unit_price,
+        CAST(total_price AS FLOAT) AS total_price,
+        CAST(cogs AS FLOAT) AS cogs,
+        CAST(inventory_latest AS INT) AS inventory_latest, 
+        CAST(inventory_after AS INT) AS inventory_after
+    FROM stg_pos
+)
+SELECT * FROM rename
