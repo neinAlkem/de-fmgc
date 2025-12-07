d6b97c06df9a8b6b067e45763e351119e2b336e8
Baghas Rizaluddin <bagasrizal.work@gmail.com>
2025-12-07 18:41:21 +0700

    chore: update mockData.py startdate

--- stream/mockData.py
+++ stream/mockData.py
@@ -258,7 +258,7 @@ def generateData():
     productId, productType, product, type, price, cogs= random.choice(products)
     storeId, storeName, areaCode = random.choice(stores)
     
-    startDate = datetime(2025, 1, 1)
+    startDate = datetime(2025, 12, 1)
     endDate = datetime.today()
     
     quantity = random.randint(1,10)
